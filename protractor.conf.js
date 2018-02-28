4f0f5d2e9594d091bc1b2309cbdea23ad1bfa2a0
EduardPopenko <35812706+EduardPopenko@users.noreply.github.com>
2018-02-28 10:43:52 +0200

    protr.conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,6 @@ require('ts-node').register();
 
 module.exports.config = {
     specs: ['spec.ts', 'specs/*.ts'],
-    directConnect: true,
     baseUrl: 'https://movies-finder.firebaseapp.com/',
     SELENIUM_PROMISE_MANAGER: false,
     
@@ -10,20 +9,5 @@ module.exports.config = {
         browserName: 'chrome', 
         enableVNC: true,
         name: "Eduard" // Just to identify your session between others on selenoid ui
-    },
-    onPrepare: async function () {
-    // Global implicit wait setup
-    await browser.manage().timeouts().implicitlyWait(1000)
-     
-    afterEach(async function () {
-    await browser.manage().timeouts().implicitlyWait(1000)
-    })
-     
-    // Adding nice console output.
-    let ConsoleReporter = require('jasmine2-reporter').Jasmine2Reporter
-    let console_reporter_options = {
-    startingSpec: true
-    }
-    jasmine.getEnv().addReporter(new ConsoleReporter(console_reporter_options))
     }
     }
